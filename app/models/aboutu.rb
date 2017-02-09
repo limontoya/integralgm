80c5d56b52f933a295e897e62519cef31986bda0
Lina J Montoya <montoyalinaj@gmail.com>
2017-02-08 20:43:21 -0500

    Adding Bootstrap for UI

--- app/models/aboutu.rb
+++ app/models/aboutu.rb
@@ -1,2 +1,5 @@
 class Aboutu < ApplicationRecord
+	mount_uploader :urlnosotros, AvatarUploader
+	mount_uploader :urlmision,   AvatarUploader
+	mount_uploader :urlvision,   AvatarUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :producto_adjuntos
   resources :productos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'aboutus#index'
 end
