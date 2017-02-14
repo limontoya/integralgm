ec1f2c62a62ea6011e84214b7497c167821c837b
Lina J Montoya <montoyalinaj@gmail.com>
2017-02-13 21:54:06 -0500

    Bootstrap additions, Search bar into products

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,14 @@
 class SearchController < ApplicationController
   def create
+  	palabra = "%#{params[:keyword]}%"
+
+  	@productos = Producto.where("nombre LIKE ? OR descripcion LIKE ? OR especificaciones LIKE ? OR caracteristicas LIKE ?", palabra,palabra,palabra,palabra).order("created_at DESC")
+  	
+  	respond_to do |format|
+  		format.html { redirect_to root_path }
+  		format.json { render @productos }
+  		format.js
+  	end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'main/index'
-
-  get 'search/create'
-
+  
   resources :aboutus
   resources :producto_adjuntos
   resources :productos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'aboutus#index'
+  get 'search/create'
+
 end
